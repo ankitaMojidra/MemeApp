6cf870012ff3d06a296b07018e056b464b507b06
ankitaMojidra <anki.mjdr721@gmail.com>
2024-12-06 06:34:46 -0600

    Initial commit

---
+++ app/src/main/java/com/example/memeapp/MemeApplication.kt
@@ -0,0 +1,7 @@
+package com.example.memeapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MemeApplication: Application()
---
+++ app/src/main/java/com/example/memeapp/di/AppModule.kt
@@ -0,0 +1,4 @@
+package com.example.memeapp.di
+
+class AppModule {
+}
